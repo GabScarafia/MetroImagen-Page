eeebd5f97eb6419c06ac8ab778d3081ee92c6fee
Gabriel Scarafia <gabriel.hugo.s@gmail.com>
2026-04-02 17:58:13 -0300

    acomodo para visual

--- datos.js
+++ datos.js
@@ -19,11 +19,11 @@ const datosWeb = {
     {
       id: "letras",
       titulo: "LETRAS",
-      portada: "img/productos/letras/01.jpeg",
+      portada: "img/productos/letras/portada.jpg",
       descripcion: "Letras corpóreas de distintos materiales y acabados.",
       media: [
-        "img/productos/letras/01.jpeg",
-        "img/productos/letras/foto2.jpg"
+        "img/productos/letras/portada.jpg",
+        "img/productos/letras/01.jpeg"
       ]
     },
     {
@@ -32,8 +32,8 @@ const datosWeb = {
       portada: "img/productos/fronts/portada.jpg",
       descripcion: "Frentes comerciales atractivos y duraderos.",
       media: [
-        "img/productos/fronts/foto1.jpg",
-        "img/productos/fronts/foto2.jpg"
+        "img/productos/fronts/portada.jpg",
+        "img/productos/fronts/01.jpeg"
       ]
     },
     {
@@ -42,38 +42,38 @@ const datosWeb = {
       portada: "img/productos/marquesinas/portada.jpg",
       descripcion: "Marquesinas de alto impacto.",
       media: [
-        "img/productos/marquesinas/foto1.jpg",
-        "img/productos/marquesinas/foto2.jpg"
+        "img/productos/marquesinas/portada.jpg",
+        "img/productos/marquesinas/01.webp"
       ]
     },
     {
       id: "carteles-backlight",
       titulo: "CARTELES BACKLIGHT",
-      portada: "img/productos/carteles-backlight/portada.jpg",
+      portada: "img/productos/carteles-backlight/portada.jpeg",
       descripcion: "Carteles luminosos backlight para noche y día.",
       media: [
-        "img/productos/carteles-backlight/foto1.jpg",
-        "img/productos/carteles-backlight/foto2.jpg"
+        "img/productos/carteles-backlight/portada.jpeg",
+        "img/productos/carteles-backlight/01.jpeg"
       ]
     },
     {
       id: "pastillas",
       titulo: "PASTILLAS",
-      portada: "img/productos/pastillas/portada.jpg",
+      portada: "img/productos/pastillas/portada.webp",
       descripcion: "Carteles tipo pastilla, ideales para salientes.",
       media: [
-        "img/productos/pastillas/foto1.jpg",
-        "img/productos/pastillas/foto2.jpg"
+        "img/productos/pastillas/portada.webp",
+        "img/productos/pastillas/01.webp"
       ]
     },
     {
       id: "vinilo-plotter",
       titulo: "VINILO/PLOTTER",
-      portada: "img/productos/vinilo-plotter/portada.jpg",
+      portada: "img/productos/vinilo-plotter/portada.jpeg",
       descripcion: "Ploteos y vinilos decorativos o institucionales.",
       media: [
-        "img/productos/vinilo-plotter/foto1.jpg",
-        "img/productos/vinilo-plotter/foto2.jpg"
+        "img/productos/vinilo-plotter/portada.jpeg",
+        "img/productos/vinilo-plotter/01.png"
       ]
     },
     {
@@ -82,18 +82,20 @@ const datosWeb = {
       portada: "img/productos/carteles-columna/portada.jpg",
       descripcion: "Carteles de gran altura con estructura de columna.",
       media: [
-        "img/productos/carteles-columna/foto1.jpg",
-        "img/productos/carteles-columna/foto2.jpg"
+        "img/productos/carteles-columna/portada.jpg",
+        "img/productos/carteles-columna/01.jpeg"
       ]
     },
     {
       id: "carteles-pie-calle",
       titulo: "CARTELES CON PIE PARA CALLE",
-      portada: "img/productos/carteles-pie-calle/portada.jpg",
+      portada: "img/productos/carteles-pie-calle/portada.jpeg",
       descripcion: "Pizarras o carteles portátiles para la vereda.",
       media: [
-        "img/productos/carteles-pie-calle/foto1.jpg",
-        "img/productos/carteles-pie-calle/foto2.jpg"
+        "img/productos/carteles-pie-calle/portada.jpeg",
+        "img/productos/carteles-pie-calle/01.jpg",
+        "img/productos/carteles-pie-calle/ej.mp4",
+        "img/productos/carteles-pie-calle/02.jpeg"
       ]
     }
   ]
