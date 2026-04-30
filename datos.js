e91206acf4e3dd073e8992ecdccd04a50cae6481
Gabriel Scarafia <gabriel.hugo.s@gmail.com>
2026-04-30 20:39:23 -0300

    avanzo 30

--- datos.js
+++ datos.js
@@ -9,9 +9,7 @@ const datosWeb = {
     ],
     mediaNosotros: [
       // Ejemplo de cómo enlazar fotos locales:
-      "img/nosotros/01.jpeg",
-      "img/nosotros/ej.mp4",
-      "img/nosotros/02.png"
+      "img/nosotros/fullfarma10.png"
       // Si tuvieras un video, lo agregas así: "img/nosotros/mivideo.mp4"
     ]
   },
@@ -23,7 +21,14 @@ const datosWeb = {
       descripcion: "Letras corpóreas de distintos materiales y acabados.",
       media: [
         "img/productos/letras/portada.jpg",
-        "img/productos/letras/01.jpeg"
+        "img/productos/letras/IMG-20250130-WA0055.jpg",
+        "img/productos/letras/IMG-20250207-WA0037.jpg",
+        "img/productos/letras/IMG-20250506-WA0110.jpg",
+        "img/productos/letras/IMG-20260214-WA0077 (1).jpeg",
+        "img/productos/letras/I_need_you_2k_202601271435.jpeg",
+        "img/productos/letras/LUBRICENTRO-2.jpg",
+        "img/productos/letras/portadaferreiro.png_202603261149.jpeg",
+        "img/productos/letras/SPORTCLUB mejorada.png"
       ]
     },
     {
@@ -33,7 +38,16 @@ const datosWeb = {
       descripcion: "Frentes comerciales atractivos y duraderos.",
       media: [
         "img/productos/fronts/portada.jpg",
-        "img/productos/fronts/01.jpeg"
+        "img/productos/fronts/20250625_184932.jpg",
+        "img/productos/fronts/front - inmobiliaria.png",
+        "img/productos/fronts/IMG-20250305-WA0073.jpg",
+        "img/productos/fronts/IMG-20250318-WA0038.jpg",
+        "img/productos/fronts/IMG01858-20131012-1656.jpg",
+        "img/productos/fronts/IMG01860-20131012-1657.jpg",
+        "img/productos/fronts/Pranavita_reel.00_00_01_24.Imagen fija001.png",
+        "img/productos/fronts/Remove_the_graffiti_2k_202601200956.jpeg",
+        "img/productos/fronts/WhatsApp Image 2025-08-08 at 13.09.52.jpeg",
+        "img/productos/fronts/WhatsApp Image 2025-10-28 at 15.14.01 (3).jpeg"
       ]
     },
     {
@@ -43,7 +57,9 @@ const datosWeb = {
       descripcion: "Marquesinas de alto impacto.",
       media: [
         "img/productos/marquesinas/portada.jpg",
-        "img/productos/marquesinas/01.webp"
+        "img/productos/marquesinas/ENALUS.png",
+        "img/productos/marquesinas/FullFarma_9.jpg",
+        "img/productos/marquesinas/IMG-20250128-WA0045.jpg"
       ]
     },
     {
@@ -59,16 +75,35 @@ const datosWeb = {
     {
       id: "pastillas",
       titulo: "PASTILLAS",
-      portada: "img/productos/pastillas/portada.webp",
+      portada: "img/productos/pastillas/portada.jpg",
       descripcion: "Carteles tipo pastilla, ideales para salientes.",
       media: [
-        "img/productos/pastillas/portada.webp",
-        "img/productos/pastillas/01.webp"
+        "img/productos/pastillas/portada.jpg",
+        "img/productos/pastillas/AFRIKA EDIT IA.png",
+        "img/productos/pastillas/EDIT PRANAVITA-2.png",
+        "img/productos/pastillas/FRATELLI EDIT.png",
+        "img/productos/pastillas/IMG-20251214-WA0020.jpg",
+        "img/productos/pastillas/pastillas3.jpg",
+        "img/productos/pastillas/PASTILLAS4.jpg",
+        "img/productos/pastillas/QIAN WEI.png",
+        "img/productos/pastillas/WhatsApp Image 2025-08-08 at 13.09.53.jpeg"
+      ]
+    },
+    {
+      id: "salientes-cubicos",
+      titulo: "SALIENTES CÚBICOS",
+      portada: "img/productos/salientes-cubicos/portada.png",
+      descripcion: "Letreros y carteles salientes de diseño cúbico.",
+      media: [
+        "img/productos/salientes-cubicos/portada.png",
+        "img/productos/salientes-cubicos/20251121_082501.jpg",
+        "img/productos/salientes-cubicos/FETTA CUBICO - EDIT2.png",
+        "img/productos/salientes-cubicos/I_need_the_2k_202601141356.jpeg"
       ]
     },
     {
       id: "vinilo-plotter",
-      titulo: "VINILO/PLOTTER",
+      titulo: "VINILO  PLOTTER",
       portada: "img/productos/vinilo-plotter/portada.jpeg",
       descripcion: "Ploteos y vinilos decorativos o institucionales.",
       media: [
@@ -79,11 +114,14 @@ const datosWeb = {
     {
       id: "carteles-columna",
       titulo: "CARTELES CON COLUMNA",
-      portada: "img/productos/carteles-columna/portada.jpg",
+      portada: "img/productos/carteles-columna/portada.jpeg",
       descripcion: "Carteles de gran altura con estructura de columna.",
       media: [
-        "img/productos/carteles-columna/portada.jpg",
-        "img/productos/carteles-columna/01.jpeg"
+        "img/productos/carteles-columna/portada.jpeg",
+        "img/productos/carteles-columna/20241217_171045.jpg",
+        "img/productos/carteles-columna/IMG-20241216-WA0003.jpg",
+        "img/productos/carteles-columna/WhatsApp Image 2025-06-23 at 13.49.53.jpeg",
+        "img/productos/carteles-columna/WhatsApp Image 2025-06-23 at 13.50.00.jpeg"
       ]
     },
     {
@@ -94,8 +132,8 @@ const datosWeb = {
       media: [
         "img/productos/carteles-pie-calle/portada.jpeg",
         "img/productos/carteles-pie-calle/01.jpg",
-        "img/productos/carteles-pie-calle/ej.mp4",
-        "img/productos/carteles-pie-calle/02.jpeg"
+        "img/productos/carteles-pie-calle/02.jpeg",
+        "img/productos/carteles-pie-calle/ej.mp4"
       ]
     }
   ]
